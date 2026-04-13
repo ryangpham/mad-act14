61e553fd9850d09b42d7b61d3ae8690202a69a1b
ryangpham <ryannjr2004@gmail.com>
2026-04-13 13:30:16 -0400

    chore: firebase setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:act14/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('App shows Firebase setup message', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(find.text('Firebase Setup'), findsOneWidget);
+    expect(
+      find.text('Firebase initialized for Android and Web.'),
+      findsOneWidget,
+    );
   });
 }
